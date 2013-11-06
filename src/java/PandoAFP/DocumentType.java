5fdb0eee7af8ad249954d6c9fcf5ddf34d2a65af
ajmiro <ajmiro@ajmiro-PC.dsg.local>
2013-11-06 14:30:43 -0500

    Removed all unused imports

--- src/java/PandoAFP/DocumentType.java
+++ src/java/PandoAFP/DocumentType.java
@@ -4,14 +4,6 @@
  */
 package PandoAFP;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.naming.NamingException;
-
 /**
  *
  * @author ajmiro
