1e9d92fb954a066bbbf9c7d3a41b6a1e95acaac6
ajmiro <ajmiro@ajmiro-PC.dsg.local>
2013-11-12 13:14:47 -0500

    Refactored. Removed comment count code and placed it in its own class.

--- src/java/PandoAFP/Candidate.java
+++ src/java/PandoAFP/Candidate.java
@@ -4,14 +4,6 @@
  */
 package PandoAFP;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.naming.NamingException;
-
 /**
  *
  * @author ajmiro
@@ -63,38 +55,7 @@ public class Candidate {
      * @return the commentCount
      */
     public int getCommentCount() {
-        Connection connection = null;
-        try {            
-            connection = getDBConnection();
-            String sql = "SELECT COUNT(ID) as 'count' FROM CandidateRanking WHERE Candidate = " + this.id;
-            PreparedStatement statement = connection.prepareStatement(sql);
-            ResultSet result = statement.executeQuery();
-            result.next();
-            commentCount = result.getInt("count");
-        } catch (SQLException ex) {
-            Logger.getLogger(Candidate.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException ex) {
-                    Logger.getLogger(Candidate.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-        }
+        commentCount = Comment.getCommentCountForCandidate(this);
         return commentCount;
-    }
-    
-    private Connection getDBConnection() {
-        Connection connection = null;
-        try {
-            Data data = new Data();
-            connection = data.createDbConnection();            
-        } catch (SQLException ex) {
-            Logger.getLogger(Candidate.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (NamingException ex) {
-            Logger.getLogger(Candidate.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return connection;
-    }
+    }    
 }
