6881c70078a50b9ed5e7a1d5b95cadfca2d8bbf5
ajmiro <ajmiro@ajmiro-PC.dsg.local>
2013-11-12 14:27:15 -0500

    Added

---
+++ src/java/PandoAFP/User.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PandoAFP;
+
+/**
+ *
+ * @author ajmiro
+ */
+public class User {
+    private String firstName;    
+    private String lastName;
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        //return firstName;
+        return "Alfredo";
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        //return lastName;
+        return "Miro";
+    }
+}
